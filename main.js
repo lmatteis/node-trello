89471bffa8701d53ec7f641344c027ae9fe96413
Luca Matteis <lmatteis@gmail.com>
2011-12-22 12:33:58 +0100

    changed some stuff adopting it to new api

--- main.js
+++ main.js
@@ -1,52 +1,59 @@
 var https = require('https');
 
 var trello = {};
+trello.key = false;
+trello.token = false;
+
+trello.params = function(args) {
+  var str = "";
+  var c = 1;
+  for(var i in args) {
+    if (c == 1)
+      c = 0;
+    else
+      str += "&";
+    str += i + "=" + args[i];
+  }
+  return str;
+};
 
 trello.api = function(apiCall, callback) {
   var host = "trello.com";
   var options = {
     host: host,
     port: 443,
-    path: '/data' + apiCall,
+    path: apiCall,
     method: 'GET'
   };
 
+  var args = {};
+  if(trello.key) {
+    args["key"] = trello.key;
+  }
+  if(trello.token) {
+    args["token"] = trello.token;
+  }
+
+  options.path += "?" + trello.params(args);
   var req = https.request(options, function(res) {
     res.setEncoding('utf8');
-    if(res.statusCode !== 200) {
-      throw "Bad status code";
-    }
     var data = "";
     res.on('data', function(d) {
       data += d;
     });
     res.on("end", function() {
-      var j = JSON.parse(data);
-      callback(j);
+      if(res.statusCode !== 200) {
+        callback(data);
+      } else {
+        var j = JSON.parse(data);
+        callback(false, j);
+      }
     });
   });
   req.end();
 
   req.on('error', function(e) {
     throw e;
-  });
-};
-
-trello.get = function(org, callback) {
-  trello.api("/" + org, function(data) {
-    var len = data.boards.length;
-    var done = 0;
-    var ret = [];
-    data.boards.forEach(function(i, idx) {
-      var id = i._id;
-      trello.api("/board/" + id + "/current", function(data) {
-        done++;
-        ret.push(data.boards[0]);
-        if(done === len) { // last
-          callback(ret);
-        }
-      });
-    });
   });
 };
 
--- tests.js
+++ tests.js
@@ -1,22 +1,24 @@
 var assert = require('assert'),
     trello = require('./main.js');
 
+// https://trello.com/docs/api/index.html
 var tests = {
+  testParams: function(){
+    var args = { foo: "bar", "french":"noob"};  
+    var query = trello.params(args);
+    var expected = "foo=bar&french=noob";
+    assert.equal(query, expected, query + " should be " + expected);
+  },
   getBoards: function() {
-    var t = trello.api("/grinfo", function(data) {
-      assert.ok(data.boards.length > 0); 
-    });
-  },
-  getBoardsDescription: function() {
-    var t = trello.api("/board/4e858daf5e1715cc88aa95a1/current", function(data) {
-    });
-  },
-  getEssentialStructure: function() {
-    trello.get("grinfo", function(data) {
-      console.log(data);
+    trello.api("/1/organization/grinfo/boards/all", function(err, data) {
+      if(err) throw err;
+      assert.ok(data.length > 0);
     });
   }
 };
+
+trello.key = process.argv[2];
+trello.token = process.argv[3];
 
 for(var i in tests) {
   tests[i]();
