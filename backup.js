cee09ca68cc1225c04de3a0f2d04e22f65009689
Matthias Luebken <matthias@luebken.com>
2012-01-06 23:44:08 +0100

    basic csv support

--- backup.js
+++ backup.js
@@ -1,9 +1,10 @@
 var Trello = require("./main.js");
 var fs = require('fs');
 
-var trello_backup = function(app_key, oauth_access_token, organization) {
+var trello_backup = function(app_key, oauth_access_token, organization, data_type) {
     this.organization = organization;
     this.trello = new Trello(app_key, oauth_access_token);
+    this.data_type = data_type; //json, csv
 }
 
 trello_backup.prototype.backupOrganization = function() {
@@ -17,19 +18,57 @@ trello_backup.prototype.backupOrganization = function() {
 }
 
 trello_backup.prototype.backupCards = function(board_id, board_name) {
+    var self = this;
     this.trello.get('/1/board/' + board_id + '/cards/all', function(err, data) {
         if(err) throw err;
-        var filename = board_name + " - " + new Date().toString()  + ".json";
-        console.log('Backing up ' + data.length + ' cards for board "' + board_name + '"');
-
-        fs.writeFile(filename, JSON.stringify(data), function(err) {
-            if(err) {
-                console.log(err);
-            } else {
-                console.log("Saved to " + filename);
-            }
-        });
+        self.writeCards(board_name, data);
     });
+}
+
+trello_backup.prototype.writeCards = function(board_name, data) {
+    var filename = board_name + " - " + new Date().toString()  + "." + this.data_type;
+    console.log('Backing up ' + data.length + ' cards for board "' + board_name + '"');
+
+    var string;
+    if(this.data_type == 'csv') {
+        string = this.createCSV(data);
+    }else {
+        string = JSON.stringify(data);
+    }
+
+    fs.writeFile(filename, string, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Saved to " + filename);
+        }
+    });
+}
+
+trello_backup.prototype.createCSV = function(data) {
+    var csv = "";
+    //title
+    var card = data[0];
+    for(key in card) {
+        csv += key + ", ";
+    }
+    csv = csv.substr(0, csv.length-2);
+    csv += '\n';
+
+    //data
+    for (var i=0; i < data.length; i++) {
+        var card = data[i];
+        for(key in card) {
+            var prop = card[key].toString();
+            prop = prop.replace(/"/g,' '); // remove "
+            prop = prop.replace(/,/g,' '); // remove ,
+            prop = prop.replace(/\n/g, ' '); // remove new lines
+            csv += prop + ", ";
+        }
+        csv = csv.substr(0, csv.length-2);
+        csv += '\n';
+    };
+    return csv;
 }
 
 exports = module.exports = trello_backup;
--- example_backup.js
+++ example_backup.js
@@ -1,11 +1,13 @@
 var TrelloBackup = require("./backup.js");
 
+
 //config
 var app_key = '<app_key>';
 var oauth_access_token = '<oauth_access_token>';
 var organization = '<organization>';
+var data_type = 'csv'; //json, csv
 
 //usage
-var tb = new TrelloBackup(app_key, oauth_access_token, organization);
+var tb = new TrelloBackup(app_key, oauth_access_token, organization, data_type);
 tb.backupOrganization();
 
