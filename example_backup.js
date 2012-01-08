fb81e716709de5aa43b7c315589c29d507d34c6b
David Johnson <dwj@cypress.com>
2012-01-08 02:11:33 -0800

    Revising example to backup lists and cards for multiple boards
    This copies heavily from the revised code from adcloud to correct the problem
    with multiple board backup corrupting the base filename and overwriting backups.

--- example_backup.js
+++ example_backup.js
@@ -7,22 +7,24 @@ var Trello = require("./main.js");
 var t = new Trello(app_key, oauth_access_token);
 
 t.get("/1/organization/" + organization + "/boards/all", function(err, data) {
-  if(err) throw err;
-  for(board in data) {
-      var board_id = data[board].id;
-      var board_name = data[board].name;
-      t.api('/1/board/' + board_id + '/cards/all', function(err, data) {
-          if(err) throw err;
-          var filename = board_name + " - " + new Date().toString()  + ".json";
-          console.log('Backing up ' + data.length + ' cards for board "' + board_name);
-          
-          fs.writeFile(filename, JSON.stringify(data), function(err) {
-              if(err) {
-                  console.log(err);
-              } else {
-                  console.log("Saved to " + filename);
-              }
-          });
-      });
-  }
+    if(err) throw err;
+    for (var i=0; i < data.length; i++) {
+        backupLists(data[i].id, data[i].name);
+    }
 });
+
+function backupLists(board_id, board_name) {
+    t.get('/1/board/' + board_id + '/lists/all', function(err, data) {
+        if(err) throw err;
+        var filename = board_name + " - " + new Date().toString() + ".json";
+        console.log('Backing up ' + data.length + ' cards for board "' + board_name + '"');
+
+        fs.writeFile(filename, JSON.stringify(data), function(err) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log("Saved to " + filename);
+            }
+        });
+    });
+}
