cd3250a0bb093ce3322b3be427f45de256e29c48
Matthias Luebken <matthias@luebken.com>
2012-01-11 11:12:12 +0100

    wrong variable name

--- example_stats.js
+++ example_stats.js
@@ -4,9 +4,9 @@ var TrelloStats = require("./stats.js");
 // Config
 var app_key = '<app_key>';
 var oauth_access_token = '<oauth_access_token>';
-var organization = '<organization>';
+var board_id = '<board_id>';
 
 // Extract status from your boards
 // Usage
-var tb = new TrelloStats(app_key, oauth_access_token, organization);
+var tb = new TrelloStats(app_key, oauth_access_token, board_id);
 tb.createStats();
