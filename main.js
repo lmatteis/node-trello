dcdfa918fcb3939bd4d85c0e4c53c6cd175ca951
Matthias Luebken <matthias@luebken.com>
2012-03-28 09:44:21 +0200

    rename to stats

---
+++ main.js
@@ -0,0 +1,11 @@
+var stats = require('./stats');
+
+
+//config
+
+var api_key = '<api_key>';
+var api_token = '<api_token';
+var organization = '<organization>';
+
+//usage
+stats.createStats(api_key, api_token, organization);
