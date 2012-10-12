d1ec7dbe13ad75153aa2125b5f21603010d12ebe
Waldemar Schwan <waldemar.schwan@adcloud.com>
2012-10-12 11:59:55 +0200

    yes

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Trello Stats ist das coolste Projekt!');
+  response.send('Trello Stats ist das coolste Projekt, EVER!');
 });
 
 var port = process.env.PORT || 5000;
