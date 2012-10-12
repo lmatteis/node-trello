105088fcbfbaf8843659315014a33ea7107f31a4
Waldemar Schwan <waldemar.schwan@adcloud.com>
2012-10-12 16:42:06 +0200

    switched port back to 5000

--- web.js
+++ web.js
@@ -28,7 +28,7 @@ app.get('*', function(request, response) {
 	response.sendfile("./public/" + request.route.params[0]);
 });
 
-var port = process.env.PORT || 5001;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
