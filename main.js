ca2b31acb071f4f4b139e0f8f13913d4e6f600fb
Matthias Luebken <matthias.luebken@gmail.com>
2012-10-12 14:41:58 +0200

    removed main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+var stats = require('./stats');
+var fs = require('fs');
+
+
+//config
+var api_key = process.env.TRELLO_API_KEY;
+//https://trello.com/1/authorize?key=4b65fb9aa0db5d712f29917366c6edf7&name=My+Application&expiration=30days&response_type=token
+var api_token = process.env.TRELLO_API_TOKEN;
+
+var boards = [];
+boards.push({ board_name : "A Team", board_id: '4f681edb801cba2d41140478'})
+boards.push({ board_name : "Team 404", board_id: '4f05b23a7bd9872d743ece95'})
+boards.push({ board_name : "Team XXX", board_id: '50350ea44ac40fb64b0044f4'})
+
+
+var writeFile = function(data, callback) {
+	var filename = 'backup-data/' + new Date().toString()  + ".csv";
+    fs.writeFile(filename, data, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("\nSaved to " + filename);
+        }
+        callback(null);
+    });
+}
+
+//usage
+stats.createStats(api_key, api_token, boards, writeFile);
