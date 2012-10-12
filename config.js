1562d7c35ef865a7435b792629a750c8deead1dd
Waldemar Schwan <waldemar.schwan@adcloud.com>
2012-10-12 14:58:20 +0200

    throwing error inset of log messages, because the application can not run without the reqired trello env vars anyways.

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ var api_key, api_token;
 if (process.env.TRELLO_API_KEY) {
 	api_key = process.env.TRELLO_API_KEY;
 } else {
-	console.log("[ERROR] env var TRELLO_API_KEY not found! Using empty string.");
+	throw new Error("Environment variable (TRELLO_API_KEY) not set, but required!");
 	api_key = "";
 }
 
 if (process.env.TRELLO_API_TOKEN) {
 	api_token = process.env.TRELLO_API_TOKEN;
 } else {
-	console.log("[ERROR] env var TRELLO_API_TOKEN not found! Using empty string.");
+	throw new Error("Environment variable (TRELLO_API_TOKEN) not set, but required!");
 	api_token = "";
 }
 
