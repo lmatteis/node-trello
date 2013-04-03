9fc632ef5bfe447eaa5b9231360955f3281109c5
Matthias Luebken <matthias.luebken@gmail.com>
2013-04-03 23:03:23 +0200

    csv export

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var stats = require('./lib/stats');
+var csv = require('./lib/csv');
 var fs = require('fs');
 var config = require('./config');
 
@@ -12,9 +13,12 @@ var boards = [];
 boards.push({ board_name : "A Team", board_id: '4f681edb801cba2d41140478'})
 boards.push({ board_name : "Team 404", board_id: '4f05b23a7bd9872d743ece95'})
 boards.push({ board_name : "Team XXX", board_id: '50350ea44ac40fb64b0044f4'})
+boards.push({ board_name : "Better Results", board_id: '512231a0225e76054a0053e5'})
+boards.push({ board_name : "Connect", board_id: '512223f1857953fd79005f7e'})
+boards.push({ board_name : "Product Experience", board_id: '5122243fa7b55d005c00510a'})
 
 
-var writeFile = function(data, callback) {
+var writeFile = function(err, data, callback) {
 	var filename = 'backup-data/' + new Date().toString()  + ".csv";
     fs.writeFile(filename, data, function(err) {
         if(err) {
@@ -22,9 +26,13 @@ var writeFile = function(data, callback) {
         } else {
             console.log("\nSaved to " + filename);
         }
-        callback(null);
+        //callback(null);
     });
 }
 
+var cb = function(data, callback) {
+	csv.convertToCSV(data, writeFile)
+}
+
 //usage
-stats.createStats(api_key, api_token, boards, writeFile);
+stats.createStats(api_key, api_token, boards, cb);
